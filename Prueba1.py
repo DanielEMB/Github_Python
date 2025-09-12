a8f6fe7d95f03396d4393120f5b1a12105b47ecb
DanielEMB <danielenric.meza@campus.xaloc.org>
2025-09-12 16:03:23 +0200

    Prueba 1
    Esta es la primera prueba de todas que hicimos

---
+++ Prueba1.py
@@ -0,0 +1,10 @@
+#variable numérica
+var_1 = 4
+#variable decimal
+var_2 = 4.5
+#variable texto
+var_3 = "hola"
+
+print("la variable 1 es..", var_1)
+print("la variable 2 es..", var_2)
+print("la variable 3 es..", var_3)
