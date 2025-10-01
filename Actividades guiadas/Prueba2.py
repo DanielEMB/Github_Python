90b23c96019c4982729f59a20850df0662a716cd
DanielEMB <danielenric.meza@campus.xaloc.org>
2025-10-01 09:17:33 +0200

    Python

---
+++ Actividades guiadas/Prueba2.py
@@ -0,0 +1,12 @@
+#textososososo:)
+
+var1 = 3
+var2 = 3.4
+var3 = "total"
+var4 = 5
+#total = var1 + var4
+
+var1 = int(input("Introduce un número para la var 1"))
+var2 = int(input("Introduce un número para la var 2"))
+total = var1 + var2
+print(f"el total de (var1) + (var4) es: ",total," euros")
