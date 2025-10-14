b0ba9f008dcd3f80f5b56fcb224a15996b3645c1
DanielEMB <danielenric.meza@campus.xaloc.org>
2025-10-14 10:51:13 +0200

    Python

---
+++ Simulacro examen UD1.py/ejercicio2.py
@@ -0,0 +1,14 @@
+frase= input("Introduce una frase")
+numero1 = float(input("Introduce un número1"))
+numero2 = float(input("Introduce un número2"))
+numero3 = float(input("Introduce un número3"))
+
+#apartado1
+frase = frase.lower()
+
+#apartado2
+totalsuma = numero1 + numero2 + numero3
+totalmedia = totalsuma / 3
+redondearmedia = round(totalmedia,2)
+
+#apartado3
