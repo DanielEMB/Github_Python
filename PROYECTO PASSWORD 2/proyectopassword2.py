6c99fd40c33ed5e14bd1f514e5d930a1826f858f
DanielEMB <danielenric.meza@campus.xaloc.org>
2025-12-10 10:00:19 +0100

    Python

---
+++ PROYECTO PASSWORD 2/proyectopassword2.py
@@ -0,0 +1,9 @@
+#MODIFICACIONES PENDIENTES para PASSWORD 2:
+
+print("- 3 números(distinguiendo rangos)"),print("- 3 letras(distinguiendo mtus y minus)"),print("- 2 símbolos.")
+print("Bucle que recorra el password y realice las comprobaciones y los contajes necesarios.")
+print("introducir 3 password en total, salida que aparezca el núm de contraseñas correctas e incorrectas.")
+print("Testeo de 10 pruebas que permita la evaluación del código.")
+# Tienes la libertad de hacer las modificaciones que quieras, siempre que las indiques claramente.
+# Entrega límite para Viernes 20.
+
