1dde84a77380880553977dec67648130e93bdcae
DanielEMB <danielenric.meza@campus.xaloc.org>
2026-02-03 10:46:50 +0100

    Python

--- EJERCICIO DNI/Ejercicio DNI_DanielMeza.py
+++ EJERCICIO DNI/Ejercicio DNI_DanielMeza.py
@@ -14,10 +14,18 @@ respuesta = "s"
 
 dni_correctos = []
 dni_incorrectos = []
-errores = 0
+erroreslongitud = 0
+erroresnumerico = 0
+errorestotal = 0
 num_dnicorrectos = []
 while respuesta == "s":
     dni = input("Introduce solo los números de tu dni")
+    if len(dni) != 8:
+        print("El DNI no cumple la longitud de 8 dígitos.")
+        lista_dni.append(0)
+        dni_incorrectos.append(dni)
+        erroreslongitud += 1
+        errorestotal += 1
     if dni.isnumeric():
         if len(dni) == 8:
             resto = int(dni) % 23
@@ -26,26 +34,21 @@ while respuesta == "s":
                     dni +=  "-" + letras[i]
             lista_dni.append(3)
             dni_correctos.append(dni)
+            print("¡DNI correcto!")
     else:
         lista_dni.append(1)
-        dni_incorrectos(dni)
+        dni_incorrectos.append(dni)
         print("El dni introducido tiene que ser numérico.")
-        errores += 1
-    if len(dni) != 8:
-        print("El DNI no cumple la longitud de 8 dígitos.")
-        lista_dni.append(0)
-        dni_incorrectos(dni)
-        errores += 1
-
-    
-    respuesta = input("¿Quieres introducir otro DNI? s(n )")
+        erroresnumerico += 1
+        errorestotal += 1
+    respuesta = input("¿Quieres introducir otro DNI? s/n: ")
 dni_correctos.sort()
 dni_incorrectos.sort()
 # imprimir dni correctos, incorrectos, num de errores total, num total de dni correctos, % de DNI correctos,
 # incorrectos, errores de longitud, errores de número, no existentes.
 print("RESULTADOS. Escoge una opción."), print("1. listar DNI correcto ordenado de menor a mayor")
 print("2. Listar DNI incorrecto ordenado de menor a mayor"), print("3. Número total de errores producidos")
-print("4. Número total de DNI correctos"), print("5. Porcentajes intencos con error y sin error"),
+print("4. Número total de DNI correctos"), print("5. Porcentajes intentos con error y sin error"),
 print("6. Salir s/n")
 opción = int(input("Escoje una opción para mostrarse en la salida 1-7"))
 if opción == 1:
@@ -53,3 +56,14 @@ if opción == 1:
 if opción == 2:
     print("Los DNI incorrectos de menor a mayor son:", dni_incorrectos)
 if opción == 3:
+    print(f"Has tenido {errorestotal} errores")
+if opción == 4:
+    aciertos = len(dni_correctos)
+if opción == 5:
+    aciertos = len(dni_correctos)
+    errores = len(dni_incorrectos)
+    intentos = errores + aciertos
+    correctos = aciertos/ intentos * 100
+    incorrectos = errores / intentos * 100
+    print("El porcentaje de DNI correctos es: ", correctos)
+    print("El porcentaje de incorrectos es: ", incorrectos)
