03bfe603ac4330b7571be3bb9dad1688cb530f6d
DanielEMB <danielenric.meza@campus.xaloc.org>
2026-02-27 16:03:33 +0100

    Python

--- PROYECTO AHORCADO Daniel Meza/Ahorcado.py
+++ PROYECTO AHORCADO Daniel Meza/Ahorcado.py
@@ -8,15 +8,36 @@ palabra = random.choice(lista_palabrasecreta)
 for i in range(len(palabra)):
     lista_partida += "_"
 print(lista_partida)
-
+confirmador = 0
 while lista_partida != palabra:
     letra = input("Introduce una letra")
     for i in range(len(palabra)):
         if palabra[i] == letra:
             print("Letra encontrada:", letra, "en posición", i)
             lista_partida[i] = letra
-    if not letra in palabra:
+        elif letra == "a":
+            confirmador == 1
+            if "á" in letra:
+                lista_partida[i] = "á"
+        elif letra == "e":
+            confirmador == 1
+            if "é" in letra:
+                lista_partida[i] = "é"
+        elif letra == "i":
+            confirmador == 1
+            if "í" in letra:
+                lista_partida[i] = "í"
+        elif letra == "o":
+            confirmador == 1
+            if "ó" in letra:
+                lista_partida[i] = "ó"
+        elif letra == "u":
+            confirmador == 1
+            if "ú" in letra:
+                lista_partida[i] = "ú"
+    if not letra in palabra and confirmador == 0:
         lista_ahorcado.append(letra)
         print("Las letras incorrectas son:", lista_ahorcado)
+    confirmador == 0
     print(lista_partida)
 
